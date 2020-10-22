1b7b41ff49b4cb0205317ad5bb2e69d25715e561
Anish Govind <anishg24@gmail.com>
2020-10-22 16:01:30 -0700

    Updated webserver

--- webserver/setup.sh
+++ webserver/setup.sh
@@ -1,3 +1,4 @@
+pip3 install -r requirements.txt
 python3 service_generator.py
 cp ZoomSignWebserver.service /lib/systemd/system/ZoomSignWebserver.service
 sudo chmod 644 /lib/systemd/system/ZoomSignWebserver.service
